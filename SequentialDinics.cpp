618eb041d749a01b583e31f6135542eb61ae31b1
Connor Mowry <crcmowry@gmail.com>
2024-11-14 14:03:24 -0500

    Readded buffered reading

--- SequentialDinics.cpp
+++ SequentialDinics.cpp
@@ -81,32 +81,56 @@ public:
     }
 };
 
+// Buffered input for faster reading
+const int BUFFER_SIZE = 1 << 20; // 1 MB buffer
+char buffer[BUFFER_SIZE];
+size_t buffer_pos = 0, buffer_len = 0;
+
+inline char get_char() {
+    if (buffer_pos == buffer_len) {
+        buffer_len = fread(buffer, 1, BUFFER_SIZE, stdin);
+        buffer_pos = 0;
+    }
+    return buffer[buffer_pos++];
+}
+
+inline int fast_read_int() {
+    int x = 0;
+    char c = get_char();
+    while (c < '0' || c > '9') c = get_char(); // Skip non-digit characters
+    while (c >= '0' && c <= '9') {
+        x = x * 10 + (c - '0');
+        c = get_char();
+    }
+    return x;
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         cout << "Usage: " << argv[0] << " <input file>" << endl;
         return 1;
     }
 
-    // Start measuring initialization time with nanosecond precision
+    // Start measuring initialization time
     auto init_start = high_resolution_clock::now();
 
-    // Read input from the specified file
-    ifstream infile(argv[1]);
-    if (!infile) {
+    // Use freopen to redirect stdin to the input file
+    if (freopen(argv[1], "r", stdin) == nullptr) {
         cerr << "Error: Could not open file " << argv[1] << endl;
         return 1;
     }
 
-    int n, m;
-    infile >> n >> m;
+    int n = fast_read_int();
+    int m = fast_read_int();
     Dinic dinic(n);
 
-    int source, sink;
-    infile >> source >> sink;
+    int source = fast_read_int();
+    int sink = fast_read_int();
 
     for (int i = 0; i < m; ++i) {
-        int u, v, capacity;
-        infile >> u >> v >> capacity;
+        int u = fast_read_int();
+        int v = fast_read_int();
+        int capacity = fast_read_int();
         dinic.addEdge(u, v, capacity);
     }
 
@@ -115,7 +139,7 @@ int main(int argc, char* argv[]) {
     double init_time = duration_cast<nanoseconds>(init_end - init_start).count();
     cout << "Initialization Time: " << init_time << " nanoseconds" << endl;
 
-    // Start measuring computation time with nanosecond precision
+    // Start measuring computation time
     auto comp_start = high_resolution_clock::now();
 
     // Compute the maximum flow
@@ -126,7 +150,7 @@ int main(int argc, char* argv[]) {
     double comp_time = duration_cast<nanoseconds>(comp_end - comp_start).count();
     cout << "Computation Time: " << comp_time << " nanoseconds" << endl;
 
-    // Output the maximum flow
+    // Output the maximum flow result
     cout << "Maximum Flow: " << max_flow << endl;
 
     return 0;
